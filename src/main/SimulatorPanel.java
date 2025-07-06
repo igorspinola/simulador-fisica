a877511b84877fca9950ad445d483022f4960bf4
igorspinola <igorspinoladev@gmail.com>
2025-07-06 15:19:31 -0300

    bigger screen

--- src/main/SimulatorPanel.java
+++ src/main/SimulatorPanel.java
@@ -13,8 +13,10 @@ public class SimulatorPanel extends JPanel implements Runnable {
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale;
-    public final int maxScreenCol = 16;
-    public final int maxScreenRow = 12;
+//    public final int maxScreenCol = 16;
+//    public final int maxScreenRow = 12;
+    public final int maxScreenCol = 32;
+    public final int maxScreenRow = 20;
     public final int screenWidth = tileSize * maxScreenCol;
     public final int screenHeight = tileSize * maxScreenRow;
 
